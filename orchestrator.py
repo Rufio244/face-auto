1aa827c2debe7eeffc258e681c9d167174aac1c2
Rufio244 <thanvas9991@gmail.com>
2026-05-07 10:58:14 +0700

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -1,1 +1,13 @@
+class Orchestrator:
 
+    def build(self, task):
+
+        workflow = []
+
+        if "email" in task:
+            workflow.append("email_api")
+
+        if "identity" in task:
+            workflow.append("identity_api")
+
+        return workflow
