d933ccb89c9011c66b16244848f862fcdca4631a
Rufio244 <thanvas9991@gmail.com>
2026-05-07 10:57:42 +0700

    Implement Registry class for function storage

--- registry.py
+++ registry.py
@@ -1,1 +1,16 @@
+class Registry:
 
+    def __init__(self):
+        self.store = {}
+
+    def register(self, name, func):
+        self.store[name] = func
+
+    def get(self, name):
+        return self.store.get(name)
+
+    def exists(self, name):
+        return name in self.store
+
+
+registry = Registry()
