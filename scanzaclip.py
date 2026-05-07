58f7273c539da2b97fbe912c4312b19009f7ff11
Rufio244 <thanvas9991@gmail.com>
2026-05-07 11:00:03 +0700

    Add scanzaclip function to classify tasks

--- scanzaclip.py
+++ scanzaclip.py
@@ -1,1 +1,9 @@
+def scanzaclip(task):
 
+    if "email" in task:
+        return {"type": "email_api", "score": 0.9}
+
+    if "identity" in task:
+        return {"type": "identity_api", "score": 0.8}
+
+    return {"type": "generic", "score": 0.5}
