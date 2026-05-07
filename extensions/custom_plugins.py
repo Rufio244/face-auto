c84484a918e2b8bd25e75144684512600f759dc1
Rufio244 <thanvas9991@gmail.com>
2026-05-07 11:01:44 +0700

    Update custom_plugins.py

--- extensions/custom_plugins.py
+++ extensions/custom_plugins.py
@@ -1,1 +1,11 @@
+from registry import registry
 
+# ผู้ใช้เพิ่ม logic เองได้โดยไม่ต้องแก้ core
+
+def custom_api(data):
+    return {
+        "custom": True,
+        "input": data
+    }
+
+registry.register("custom_api", custom_api)
