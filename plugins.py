5818e4162993b907896f37da6fa4f1b6a229420a
Rufio244 <thanvas9991@gmail.com>
2026-05-07 11:00:33 +0700

    Add email_api and identity_api functions

--- plugins.py
+++ plugins.py
@@ -1,1 +1,15 @@
+from registry import registry
 
+def email_api(data):
+    return f"{data['name'].lower()}@gmail.com"
+
+
+def identity_api(data):
+    return {
+        "name": data["name"],
+        "status": "created"
+    }
+
+# auto register
+registry.register("email_api", email_api)
+registry.register("identity_api", identity_api)
