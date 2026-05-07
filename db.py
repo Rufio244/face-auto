7a04120a5d51c13435fdfc5f1e8143b29ef74959
Rufio244 <thanvas9991@gmail.com>
2026-05-07 11:01:06 +0700

    Initialize SQLite database and create registry table

--- db.py
+++ db.py
@@ -1,1 +1,17 @@
+import sqlite3
 
+DB = "faceauto.db"
+
+def init_db():
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS registry (
+        name TEXT,
+        version TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
